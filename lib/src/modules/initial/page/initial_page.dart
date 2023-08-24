c486a70faba9502ef8ef699469b2bdce80e278ab
Lucas Lermen <lermenlucas@outlook.com>
2023-08-23 22:42:33 -0300

    Ajustei timer do initial_page.dart, e foi feita a implementação da denpendencia do Dio.

--- lib/src/modules/initial/page/initial_page.dart
+++ lib/src/modules/initial/page/initial_page.dart
@@ -5,7 +5,6 @@ import 'package:my_money/src/router/app_router.dart';
 import 'package:my_money/src/shared/colors/app_colors.dart';
 import 'package:my_money/src/shared/components/app_logo_title.dart';
 
-
 class InitialPage extends StatefulWidget {
   const InitialPage({super.key});
 
@@ -14,27 +13,28 @@ class InitialPage extends StatefulWidget {
 }
 
 class _InitialPageState extends State<InitialPage> {
-  void redirect(BuildContext context){
-    Timer(const Duration(seconds: 5), (){
+  void redirect(BuildContext context) {
+    Timer(const Duration(seconds: 2), () {
       Navigator.of(context).pushReplacementNamed(AppRouter.login);
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    redirect(context);
     return Scaffold(
       backgroundColor: AppColors.initialPageBackground,
       body: SafeArea(
           child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: const [
-                AppLogoTitle(title: 'My Money'),
-              ],
-            ),
-          )),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            AppLogoTitle(title: 'My Money'),
+          ],
+        ),
+      )),
     );
   }
 }
